056872e0eb70a2b51389a0077d7240136789da38
Ilan Gibson <ilanjgibson@gmail.com>
2026-05-19 19:56:06 -0600

    reuse code and resolve issue #24

--- pacman.c
+++ pacman.c
@@ -1,5 +1,6 @@
 #include "pacman.h"
 #include "board.h"
+#include "endgame.h"
 #include <errno.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -49,10 +50,7 @@ void process_keypress(void) {
 
   switch (c) {
   case ('q'):
-    write(STDOUT_FILENO, "\x1b[2J", 4);
-    write(STDOUT_FILENO, "\x1b[H", 3);
-
-    write(STDOUT_FILENO, "\x1b[?25h", 6);
+    endgame();
     exit(0);
     break;
 
